71aff3049f99168d6f791c7d0229a2abf55cb98b
Bruce <bruce.hillsworth@gmail.com>
2023-06-19 21:34:47 -0600

    Save styling to variables for reuse without repetition

--- client/src/app/navbar.js
+++ client/src/app/navbar.js
@@ -5,57 +5,50 @@ import { useState } from "react";
 export default function Navbar() {
   const [active, setActive] = useState(false);
 
+  const navStyling = "flex justify-between bg-zinc-500 border-b-4";
+  const ulStyling = "flex items-end space-x-6 text-namaka-blue text-lg pr-6";
+  const linkStyling = "hover:text-namaka-red active:text-namaka-red";
+  const logoStyling = "hover:opacity-50 active:opacity-50 cursor-pointer pl-2";
+
   return active ? (
-    <nav className="flex justify-between bg-zinc-400 border-b-4">
+    <nav className={navStyling}>
       <Image
         src="/namaka-transparent.svg"
         alt="Nakama Logo"
         width={100}
         height={100}
-        className="hover:opacity-50 active:opacity-50 cursor-pointer pl-2"
+        className={logoStyling}
       />
-      <ul className="flex items-end space-x-6 text-namaka-blue text-lg pr-6">
+      <ul className={ulStyling}>
         <li>
-          <a
-            href="/myaccount"
-            className="hover:text-namaka-red active:text-namaka-red"
-          >
+          <a href="/myaccount" className={linkStyling}>
             My Account
           </a>
         </li>
         <li>
-          <a
-            href="/messageboard"
-            className="hover:text-namaka-red active:text-namaka-red"
-          >
+          <a href="/messageboard" className={linkStyling}>
             Message Board
           </a>
         </li>
       </ul>
     </nav>
   ) : (
-    <nav className="flex justify-between bg-zinc-400 border-b-4">
+    <nav className={navStyling}>
       <Image
         src="/namaka-transparent.svg"
         alt="Nakama Logo"
         width={100}
         height={100}
-        className="hover:opacity-50 active:opacity-50 cursor-pointer pl-2"
+        className={logoStyling}
       />
-      <ul className="flex items-end space-x-6 text-namaka-blue text-lg pr-6">
+      <ul className={ulStyling}>
         <li>
-          <a
-            href="/myaccount"
-            className="hover:text-namaka-red active:text-namaka-red"
-          >
+          <a href="/myaccount" className={linkStyling}>
             Login
           </a>
         </li>
         <li>
-          <a
-            href="/messageboard"
-            className="hover:text-namaka-red active:text-namaka-red"
-          >
+          <a href="/messageboard" className={linkStyling}>
             Sign Up
           </a>
         </li>
