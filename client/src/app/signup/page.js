ddb5ad7af3c4bc27698cb28a83cad4c92eb47241
Bruce <bruce.hillsworth@gmail.com>
2023-06-20 15:12:11 -0600

    Add validations for email and state on User model

--- client/src/app/signup/page.js
+++ client/src/app/signup/page.js
@@ -1,5 +1,28 @@
 "use client";
 import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .required("Required")
+    .min(8, "Must be at least 8 characters")
+    .max(20, "Must be 20 characters or less"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .required("Required")
+    .min(8, "Must be at least 8 characters")
+    .max(20, "Must be 20 characters or less"),
+  confirmPassword: Yup.string()
+    .required("Required")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
+  city: Yup.string()
+    .required("Required")
+    .min(2, "Must be at least 2 characters")
+    .max(50, "Must be 50 characters or less"),
+  state: Yup.string()
+    .required("Required")
+    .length(2, "State code must be 2 characters"),
+});
 
 export default function Signup() {
   const formStyling =
@@ -9,19 +32,40 @@ export default function Signup() {
     "border-4 border-namaka-blue rounded-md px-4 py-2 mb-4 w-full";
   const buttonStyling =
     "bg-zinc-500 text-namaka-blue px-4 py-2 rounded-md hover:text-namaka-red transition-all duration-200 border-2 border-namaka-blue hover:border-namaka-red";
+  const errorStyling = "bg-red-200 text-namaka-red p-2 mb-2 rounded";
 
   const formik = useFormik({
     initialValues: {
+      username: "",
       email: "",
       password: "",
+      confirmPassword: "",
+      city: "",
+      state: "",
     },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+    validationSchema: SignupSchema,
+    onSubmit: (values, { resetForm }) => {
+      alert(JSON.stringify(values));
+      resetForm();
     },
   });
 
   return (
     <form onSubmit={formik.handleSubmit} className={formStyling}>
+      <label htmlFor="username" className={labelStyling}>
+        Username
+      </label>
+      <input
+        id="username"
+        name="username"
+        type="username"
+        onChange={formik.handleChange}
+        value={formik.values.username}
+        className={inputStyling}
+      />
+      {formik.errors.username && formik.touched.username ? (
+        <div className={errorStyling}>{formik.errors.username}</div>
+      ) : null}
       <label htmlFor="email" className={labelStyling}>
         Email Address
       </label>
@@ -33,6 +77,9 @@ export default function Signup() {
         value={formik.values.email}
         className={inputStyling}
       />
+      {formik.errors.email && formik.touched.email ? (
+        <div className={errorStyling}>{formik.errors.email}</div>
+      ) : null}
       <label htmlFor="password" className={labelStyling}>
         Password
       </label>
@@ -44,6 +91,51 @@ export default function Signup() {
         value={formik.values.password}
         className={inputStyling}
       />
+      {formik.errors.password && formik.touched.password ? (
+        <div className={errorStyling}>{formik.errors.password}</div>
+      ) : null}
+      <label htmlFor="confirmPassword" className={labelStyling}>
+        Confirm Password
+      </label>
+      <input
+        id="confirmPassword"
+        name="confirmPassword"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.confirmPassword}
+        className={inputStyling}
+      />
+      {formik.errors.confirmPassword && formik.touched.confirmPassword ? (
+        <div className={errorStyling}>{formik.errors.confirmPassword}</div>
+      ) : null}
+      <label htmlFor="city" className={labelStyling}>
+        City
+      </label>
+      <input
+        id="city"
+        name="city"
+        type="city"
+        onChange={formik.handleChange}
+        value={formik.values.city}
+        className={inputStyling}
+      />
+      {formik.errors.city && formik.touched.city ? (
+        <div className={errorStyling}>{formik.errors.city}</div>
+      ) : null}
+      <label htmlFor="state" className={labelStyling}>
+        State
+      </label>
+      <input
+        id="state"
+        name="state"
+        type="state"
+        onChange={formik.handleChange}
+        value={formik.values.state}
+        className={inputStyling}
+      />
+      {formik.errors.state && formik.touched.state ? (
+        <div className={errorStyling}>{formik.errors.state}</div>
+      ) : null}
       <button type="submit" className={buttonStyling}>
         Submit
       </button>
--- server/app.py
+++ server/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
+import re
 import os
 from routes.user_routes import user_routes
+import ipdb
 
 load_dotenv()
 
@@ -21,19 +24,89 @@ app.register_blueprint(user_routes)
 class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    password_hash = db.Column(db.String(128))
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    US_STATE_CODES = {
+        "AL",
+        "AK",
+        "AZ",
+        "AR",
+        "CA",
+        "CO",
+        "CT",
+        "DE",
+        "FL",
+        "GA",
+        "HI",
+        "ID",
+        "IL",
+        "IN",
+        "IA",
+        "KS",
+        "KY",
+        "LA",
+        "ME",
+        "MD",
+        "MA",
+        "MI",
+        "MN",
+        "MS",
+        "MO",
+        "MT",
+        "NE",
+        "NV",
+        "NH",
+        "NJ",
+        "NM",
+        "NY",
+        "NC",
+        "ND",
+        "OH",
+        "OK",
+        "OR",
+        "PA",
+        "RI",
+        "SC",
+        "SD",
+        "TN",
+        "TX",
+        "UT",
+        "VT",
+        "VA",
+        "WA",
+        "WV",
+        "WI",
+        "WY",
+    }
 
-    def __init__(self, username, email, city, state):
-        self.username = username
-        self.email = email
-        self.city = city
-        self.state = state
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(254), unique=True, nullable=False)
+    password = db.Column(db.String(254))
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(2))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    last_login = db.Column(
+        db.DateTime, server_default=db.func.now(), onupdate=db.func.now()
+    )
+
+    @validates("email")
+    def validate_email(self, key, email):
+        if not re.match(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", email):
+            raise AssertionError("Provided email is not a valid email.")
+        return email
+
+    @validates("state")
+    def validate_state(self, key, state):
+        if state.upper() not in self.US_STATE_CODES:
+            raise AssertionError("Provided state is not a valid US state code.")
+        return state.upper()
+
+    def set_password(self, password):
+        self.password = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
 
 
 if __name__ == "__main__":
+    ipdb.set_trace()
     app.run(port=5555, debug=True)
