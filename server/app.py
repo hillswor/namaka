a1480756539a4de9ef30a3bf899bfcf1d2980231
Bruce <bruce.hillsworth@gmail.com>
2023-06-20 11:11:24 -0600

    Add bcrypt to pipenv.  Add username, password_hash, city, and state columns to User model

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
 import os
 from routes.user_routes import user_routes
@@ -11,14 +12,27 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
 
 app.register_blueprint(user_routes)
 
 
 class User(db.Model):
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
+    password_hash = db.Column(db.String(128))
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+
+    def __init__(self, username, email, city, state):
+        self.username = username
+        self.email = email
+        self.city = city
+        self.state = state
 
 
 if __name__ == "__main__":
