2dc032c52a5924c1b4b0db211686364fb86b789c
Bruce <bruce.hillsworth@gmail.com>
2023-06-28 19:54:55 -0600

    Style navbar

--- client/src/app/navbar.js
+++ client/src/app/navbar.js
@@ -10,14 +10,6 @@ export default function Navbar() {
 
   const router = useRouter();
 
-  const navStyling =
-    "flex justify-between bg-gray-800 text-white py-6 px-4 border-b-4 border-blue-500";
-  const ulStyling = "flex items-center space-x-6 text-lg";
-  const linkStyling =
-    "hover:text-blue-400 transition duration-200 ease-in-out cursor-pointer";
-  const logoStyling =
-    "hover:opacity-80 transition duration-200 ease-in-out cursor-pointer";
-
   const handleLogout = () => {
     fetch("/api/logout", {
       method: "DELETE",
@@ -32,52 +24,94 @@ export default function Navbar() {
   };
 
   return user ? (
-    <nav className={navStyling}>
-      <Link href="/">
-        <Image
-          src="/namaka-logo.svg"
-          alt="Nakama Logo"
-          width={100}
-          height={100}
-          className={logoStyling}
-          priority={true}
-          placeholder="blur"
-          blurDataURL="/namaka-transparent.svg"
-        />
-      </Link>
-      <ul className={ulStyling}>
-        <li>
-          <Link href={`/users/${user.id}`} className={linkStyling}>
-            My Account
-          </Link>
-        </li>
-        <li>
-          <Link href="/posts" className={linkStyling}>
-            Message Board
-          </Link>
-        </li>
-        <li onClick={handleLogout} className={linkStyling}>
-          Logout
-        </li>
-      </ul>
-    </nav>
-  ) : (
-    <nav className={navStyling}>
+    <nav
+      className={
+        "flex justify-between bg-gray-800 text-white py-2 px-4 border-b-4 border-blue-500"
+      }
+    >
       <Image
         src="/namaka-logo.svg"
         alt="Nakama Logo"
         width={100}
         height={100}
-        className={logoStyling}
+        priority={true}
+        placeholder="blur"
+        blurDataURL="/namaka-transparent.svg"
       />
-      <ul className={ulStyling}>
+      <ul className={"flex items-end space-x-6 text-lg"}>
         <li>
-          <Link href="/login" className={linkStyling}>
+          <Link
+            href="/"
+            className={
+              "hover:text-blue-400 transition duration-200 ease-in-out cursor-pointer"
+            }
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={`/users/${user.id}`}
+            className={
+              "hover:text-blue-400 transition duration-200 ease-in-out cursor-pointer"
+            }
+          >
+            My Account
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/posts"
+            className={
+              "hover:text-blue-400 transition duration-200 ease-in-out cursor-pointer"
+            }
+          >
+            Message Board
+          </Link>
+        </li>
+        <li
+          onClick={handleLogout}
+          className={
+            "hover:text-blue-400 transition duration-200 ease-in-out cursor-pointer"
+          }
+        >
+          Logout
+        </li>
+      </ul>
+    </nav>
+  ) : (
+    <nav
+      className={
+        "flex justify-between bg-gray-800 text-white py-6 px-4 border-b-4 border-blue-500"
+      }
+    >
+      <Image
+        src="/namaka-logo.svg"
+        alt="Nakama Logo"
+        width={100}
+        height={100}
+        className={
+          "hover:opacity-80 transition duration-200 ease-in-out cursor-pointer"
+        }
+      />
+      <ul className={"flex items-center space-x-6 text-lg"}>
+        <li>
+          <Link
+            href="/login"
+            className={
+              "hover:text-blue-400 transition duration-200 ease-in-out cursor-pointer"
+            }
+          >
             Login
           </Link>
         </li>
         <li>
-          <Link href="/signup" className={linkStyling}>
+          <Link
+            href="/signup"
+            className={
+              "hover:text-blue-400 transition duration-200 ease-in-out cursor-pointer"
+            }
+          >
             Sign Up
           </Link>
         </li>
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -2,11 +2,42 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <section className="flex flex-row items-top justify-center min-h-screen py-2">
-        <h1 className="text-6xl font-bold">
-          Welcome to <a href="https://nextjs.org">NĀMAKA</a>
-        </h1>
+    <main className="px-36 py-24">
+      <section className="flex flex-col items-top text-white justify-start min-h-screen py-2 bg-gray-500 px-4 py-8 rounded-lg">
+        <h1 className="text-6xl font-bold text-center">Welcome to NĀMAKA</h1>
+        <p className="mt-8 text-center">
+          Namaka is more than just an application - it's an intuitive,
+          comprehensive companion dedicated to helping you nurture and enjoy
+          your own slice of the sea. Named after the goddess of the sea, Namaka
+          brings you closer to the enchanting universe beneath the waves, right
+          at your fingertips.
+        </p>
+        <p className="mt-4 text-center">
+          With Namaka, you can easily track and manage all aspects of your
+          saltwater aquariums. Our sophisticated yet user-friendly platform
+          enables you to monitor various parameters crucial to maintaining a
+          healthy aquatic environment, including temperature, pH, salinity, and
+          much more. Each aquarium gets its dedicated dashboard, where you can
+          log information, analyze trends, and optimize care routines.
+        </p>
+        <p className="mt-4 text-center">
+          Moreover, Namaka is home to a thriving community of passionate
+          aquarists and marine life enthusiasts. Our interactive message board
+          allows you to ask questions, share your experiences, offer advice, or
+          simply engage in captivating discussions about the mesmerizing marine
+          world.
+        </p>
+        <p className="mt-4 text-center">
+          But that's not all! To access these fantastic features, all you need
+          to do is sign up and log in. Once you're part of the Namaka family,
+          you can enjoy an all-encompassing, dynamic, and supportive environment
+          that amplifies your marine adventures.
+        </p>
+        <p className="mt-4 text-center">
+          Embark on your exciting journey with Namaka today. Dive in, explore,
+          and let the sea enchant you, right in the comfort of your home!
+        </p>
+        <p className="mt-4 text-center">Welcome aboard!</p>
       </section>
     </main>
   );
